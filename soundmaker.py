dae5d4308b5560dbe185f04ba7566014d22d6a47
jcicilio <praeduco@gmail.com>
2018-03-07 20:13:13 -0500

    adding systemd service and same named python file

---
+++ soundmaker.py
@@ -0,0 +1,66 @@
+
+import sched
+import time
+import os
+from datetime import datetime
+from playsound import playsound
+from os import listdir
+from random import randint
+
+### Setup
+# set rollover time
+startTime = '08:55:00'
+endTime = '22:10:00'
+FMT = '%H:%M:%S'
+tdelta = datetime.strptime(endTime, FMT) - datetime.strptime(startTime, FMT)
+diff = tdelta.seconds
+rollover = (24 * 60 * 60) - diff
+scheduler = sched.scheduler(time.time, time.sleep)
+
+# modify depending on if running at command line or if systemd then full path is required
+path = "/home/pi/s/sounds/"
+
+soundFiles = listdir(path)
+minWaitSeconds = 5
+maxWaitSeconds = 15
+
+def Now():
+    return time.strftime(FMT, time.localtime())
+
+def playSound():
+    # https://www.nps.gov/subjects/sound/gallery.htm
+
+    index = randint(0, len(soundFiles) - 1)
+    soundName = path + soundFiles[index]
+    print "playing", soundName, Now(), startTime, endTime
+    os.system('mpg123 -a hw:1,0 -q ' + soundName + '&')
+    doSchedule()
+
+def getRand():
+    return randint(minWaitSeconds, maxWaitSeconds)
+
+def doSchedule():
+    # if at end of schedule for day
+    r = getRand()
+    if (Now() > endTime):
+        print Now(), endTime
+        print "Rolling Over"
+        r = rollover
+
+    print "next sound starting in " + str(r) + " seconds"
+    scheduler.enter(r, 1, playSound, ())
+
+if __name__ == "__main__":
+    print "soundmaker started"
+    print "setting audio levels"
+    os.system("amixer scontrols")
+    os.system("amixer sset 'PCM' 75 %")
+
+    # wait for start time to pass before beginning
+    while (Now() < startTime or Now() > endTime):
+        print "Waiting to start at ", startTime
+        time.sleep(60)
+
+    print "starting"
+    doSchedule()
+    scheduler.run()
